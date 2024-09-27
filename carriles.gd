d8688105b9c14f9b66127f6c88444277f01254db
Anais Rodríguez <anaisrodriguezcarvajal@gmail.com>
2024-09-27 18:31:21 -0300

    areas de entrada y salida en carriles

--- carriles.gd
+++ carriles.gd
@@ -2,5 +2,17 @@ extends Area2D
 
 const gravedad = 120
 	
+
+#Hace que la letra caiga
 func _process(delta):
 	position.y += gravedad * delta
+
+#Detecta un área si hay una cosa dentro
+func _on
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
