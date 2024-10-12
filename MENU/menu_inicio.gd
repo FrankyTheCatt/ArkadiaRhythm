854f5a363b00c192ab0a99ad17a5a20e90b46337
Anais Rodríguez <anaisrodriguezcarvajal@gmail.com>
2024-10-12 14:02:59 -0300

    arreglado tema carpetas
    fixed: transicion del menu

--- MENU/menu_inicio.gd
+++ MENU/menu_inicio.gd
@@ -1,14 +1,10 @@
 extends Control
 
-
-
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://MENU/Scenes/scena_juego.tscn")
-
+	get_tree().change_scene_to_file("res://BASE JUEGO/Level.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://MENU/Scenes/Opciones.tscn")
-
+	get_tree().change_scene_to_file("res://MENU/Opciones.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
