a1bae01386d677e4a8b7065014c8de0be3bbc404
JOT4JOT4 <juan.vargas01@alumnos.ucn.cl>
2024-11-20 21:20:25 -0300

    Cambios en Sprites y Menus

--- MENU/Scrips/MainMenu.cs
+++ MENU/Scrips/MainMenu.cs
@@ -8,7 +8,9 @@ public partial class MainMenu : Control
 
 	public override void _Process(double delta) { }
 	
-	public void _on_play_button_up() { }
+	public void _on_play_button_up() { 
+		GetTree().ChangeSceneToFile("res://MENU/menu_Select.tscn");
+	}
 	
 	public void _on_options_button_up() {
 		GetTree().ChangeSceneToFile("res://MENU/Opciones.tscn");
--- MENU/Scrips/Options_Menu.cs
+++ MENU/Scrips/Options_Menu.cs
@@ -7,6 +7,10 @@ public partial class Options_Menu : Node
 
 	public override void _Process(double delta) {}
 	
+	public void _on_volumen_button_up() {}
+	
+	public void _on_resolution_button_up() {}
+	
 	public void _on_back_button_up() {
 		GetTree().ChangeSceneToFile("res://MENU/menu_inicio.tscn");
 	}
